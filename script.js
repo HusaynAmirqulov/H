19fb208def4b43115cf2a1aa60f703f672a998bb
HusaynAmirqulov <amirqulovhusayn@gmail.com>
2025-04-25 08:41:24 +0500

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("loading").style.display = "none";
+
+    document.body.style.backgroundColor = "white";
+
+    document.querySelector(".m-wrapper").style.display = "block";
+  }, 3000);
+};
